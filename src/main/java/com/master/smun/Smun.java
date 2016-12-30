b18a3dffc452ea9dcbd26fb14c640a889688cc4a
bangbv <vanbang0208@gmail.com>
2016-12-30 20:11:49 +0700

    dev

--- src/main/java/com/master/smun/Smun.java
+++ src/main/java/com/master/smun/Smun.java
@@ -1,5 +1,11 @@
 package com.master.smun;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 
  * @author bangbv
@@ -7,8 +13,40 @@ package com.master.smun;
  */
 public class Smun 
 {
+ 
+	private int numOfTrans;
+	
+    void getData(String fileName, double support) throws IOException{
+    	numOfTrans = 0;
+    	Map<String, Integer> mapItemCount = new HashMap<>();
+   
+    	BufferedReader bf = new BufferedReader(new FileReader(fileName));
+    	String line;
+    	// each line (transaction) until the end of the file
+    	while((line = bf.readLine()) != null){
+    		// if the line is a comment, is empty or is a kind of metadata
+    		if(line.isEmpty() || line.charAt(0) == '#' || line.charAt(0) == '%' || line.charAt(0) == '@'){
+    			continue;
+    		}
+    		
+    		numOfTrans++;
+    		
+    		// split the line into items
+    		String[] lineSplited = line.split(" ");
+    		// for each item in the transaction
+    		for (String itemString : lineSplited) {
+				// count
+    			Integer count = mapItemCount.get(itemString);
+    			if(count == null){
+    				
+    			}else{
+    				
+    			}
+			}
+    	}
+    }
     
-    void getData(){
-    	
+    void runAlgorithm(String fileName, double support, String output) throws IOException{
+    	getData(fileName,support);
     }
 }
