5ca0ef2d21c381c6de31744717bd45eacfb66e39
bangbv <vanbang0208@gmail.com>
2016-12-31 14:51:32 +0700

    dev

---
+++ src/main/java/com/master/smun/Item.java
@@ -0,0 +1,13 @@
+package com.master.smun;
+
+public class Item {
+	public String index;
+	public int num;
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return index+":"+num;
+	}
+	
+	
+}
