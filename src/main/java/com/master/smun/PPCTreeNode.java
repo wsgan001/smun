461f2714f914b384f82e631ffe44e6567e9004c0
bangbv <vanbang0208@gmail.com>
2017-01-01 16:08:41 +0700

    dev

---
+++ src/main/java/com/master/smun/PPCTreeNode.java
@@ -0,0 +1,12 @@
+package com.master.smun;
+
+public class PPCTreeNode {
+	public int label;
+	public PPCTreeNode firstChild;
+	public PPCTreeNode rightSibling;
+	public PPCTreeNode labelSibling;
+	public PPCTreeNode father;	
+	public int count;
+	public int foreIndex;
+	public int backIndex;
+}
