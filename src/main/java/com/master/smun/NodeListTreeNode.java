25b5849de883f146c0afc603f40e3dc6217f2d89
bangbv <vanbang0208@gmail.com>
2017-03-14 19:41:51 +0700

    update

---
+++ src/main/java/com/master/smun/NodeListTreeNode.java
@@ -0,0 +1,16 @@
+package com.master.smun;
+
+/**
+ * N list
+ * @author bangbv
+ *
+ */
+public class NodeListTreeNode {
+	public int label;
+	public NodeListTreeNode firstChild;
+	public NodeListTreeNode next;
+	public int support;
+	public int NLStartinBf;
+	public int NLLength;
+	public int NLCol;
+}
