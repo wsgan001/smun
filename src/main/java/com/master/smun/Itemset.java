321b6396e64025312b42fb7dafb5c234817e53e3
Bui <bang.bui@capgemini.com>
2017-07-18 17:43:19 +0700

    add

---
+++ src/main/java/com/master/smun/Itemset.java
@@ -0,0 +1,16 @@
+package com.master.smun;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Itemset {
+	private List<Item> items = new ArrayList<Item>();
+
+	public List<Item> getItems() {
+		return items;
+	}
+
+	public void setItems(List<Item> items) {
+		this.items = items;
+	}
+}
