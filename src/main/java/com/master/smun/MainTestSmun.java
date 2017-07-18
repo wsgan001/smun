96e14376fecf8f89080a65f3d96b9aa01384400f
Bui <bang.bui@capgemini.com>
2017-07-18 16:06:01 +0700

    update

--- src/main/java/com/master/smun/MainTestSmun.java
+++ src/main/java/com/master/smun/MainTestSmun.java
@@ -3,23 +3,19 @@ package com.master.smun;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
-import java.net.URLDecoder;
 
 public class MainTestSmun {
 
 	public static void main(String[] args) throws IOException {
-		String input = readInput("input.txt");
-		String output = "output.txt";
-		
-		double minsup = 0.4;
-		
+		String input = fileToPath("input.txt");
+		String output = "output.txt";		
+		double minsup = 0.4;		
 		Smun smun = new Smun();
-		smun.runAlgorithm(input, minsup, output);
-		
+		smun.runAlgorithm(input, minsup, output);		
 	}
 
-	private static String readInput(String fileName) throws UnsupportedEncodingException {
-		URL url = MainTestSmun.class.getResource(fileName);
-		return URLDecoder.decode(url.getPath(), "UTF-8");
-	}
+    public static String fileToPath(String filename) throws UnsupportedEncodingException {
+        URL url = Smun.class.getResource(filename);
+        return java.net.URLDecoder.decode(url.getPath(), "UTF-8");
+    }	
 }
