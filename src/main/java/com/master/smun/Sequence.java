9cc3d115fbb836422fa2fec386c6181821cb766b
Bui <bang.bui@capgemini.com>
2017-07-21 17:58:12 +0700

    update

--- src/main/java/com/master/smun/Sequence.java
+++ src/main/java/com/master/smun/Sequence.java
@@ -1,9 +1,27 @@
 package com.master.smun;
 
-public class Sequence {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Sequence {
+	List<Itemset> sequence = new ArrayList<Itemset>();
 	public void addItemset(Itemset itemset) {
-		// do something		
+		sequence.add(itemset);
+	}
+	
+	public List<Itemset> getSequence() {
+		return sequence;
+	}
+	public void setSequence(List<Itemset> sequence) {
+		this.sequence = sequence;
 	}
 
+	@Override
+	public String toString() {
+		StringBuilder rs = new StringBuilder();
+		for (Itemset itemset : sequence) {
+			rs.append(itemset+" ");
+		}
+		return rs.toString();
+	}	
 }
