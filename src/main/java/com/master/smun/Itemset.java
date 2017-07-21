8c4a40b69424522fa616e7649d514e03b2bdc605
Bui <bang.bui@capgemini.com>
2017-07-21 17:57:39 +0700

    update

--- src/main/java/com/master/smun/Itemset.java
+++ src/main/java/com/master/smun/Itemset.java
@@ -5,7 +5,12 @@ import java.util.List;
 
 public class Itemset {
 	private List<Item> items = new ArrayList<Item>();
-
+	private long timestamp = 0;
+	
+	public void addItem(Item item){
+		items.add(item);
+	}
+	
 	public List<Item> getItems() {
 		return items;
 	}
@@ -13,4 +18,23 @@ public class Itemset {
 	public void setItems(List<Item> items) {
 		this.items = items;
 	}
+
+	public long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder rs = new StringBuilder();
+		rs.append("<");
+		for (Item item : items) {
+			rs.append(item+" ");
+		}
+		rs.append(">");
+		return rs.toString();
+	}	
 }
