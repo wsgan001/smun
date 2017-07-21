428a8da0ef80271bc8ca34d7bfce4e1983a5307c
Bui <bang.bui@capgemini.com>
2017-07-21 17:58:25 +0700

    update

--- src/main/java/com/master/smun/SequenceDatabase.java
+++ src/main/java/com/master/smun/SequenceDatabase.java
@@ -8,46 +8,63 @@ import java.util.List;
 import java.util.Map;
 
 public class SequenceDatabase {
-	
+
 	private List<Sequence> sequences = new ArrayList<Sequence>();
-	
-	public void scanDB1(String fileName) throws Exception{
+	private ItemFactory itemFactory = new ItemFactory();
+
+	@SuppressWarnings("resource")
+	public void scanDB1(String fileName) throws Exception {
 		Map<Integer, Integer> itemCount = new HashMap<Integer, Integer>();
 		BufferedReader br = new BufferedReader(new FileReader(fileName));
 		String line = null;
-		long timestamp;
+
 		Itemset itemset = new Itemset();
 		Sequence sequence = new Sequence();
-		
+
 		while ((line = br.readLine()) != null) {
+			long timestamp;
 			// if the line is a comment, is empty or is a kind of metadata
-			if (line.isEmpty() == true || line.charAt(0) == '#'	|| line.charAt(0) == '%' || line.charAt(0) == '@') continue;
-			
+			if (line.isEmpty() == true || line.charAt(0) == '#'
+					|| line.charAt(0) == '%' || line.charAt(0) == '@')
+				continue;
+
 			String[] lineSplited = line.split(" ");
 			for (String element : lineSplited) {
-		           if (element.codePointAt(0) == '<') {  // Timestamp
-		                String value = element.substring(1, element.length() - 1);
-		                timestamp = Long.parseLong(value);
-		                //itemset.setTimestamp(timestamp);
-		            } else if (element.equals("-1")) { // new itemset
-		                //long time = itemset.getTimestamp() + 1;
-		                sequence.addItemset(itemset);
-		                itemset = new Itemset();
-		                //itemset.setTimestamp(time);
-		            } else if (element.equals("-2")) { // end sequence
-		                sequences.add(sequence);
-		            } else {
-		                // extract the value for an item
-		                //Item item = itemFactory.getItem(Integer.parseInt(element));
-		                //Pattern pattern = frequentItems.get(item);
-/*		                if (pattern == null) {
-		                    pattern = patternCreator.createPattern(creadorPares.getItemAbstractionPair(item, abstractionCreator.CreateDefaultAbstraction()));
-		                    frequentItems.put(item, pattern);
-		                }
-		                pattern.addAppearance(sequence.getId());*/
-		                //itemset.addItem(item);
-		            }				
+				if (element.codePointAt(0) == '<') { // Timestamp
+					String value = element.substring(1, element.length() - 1);
+					timestamp = Long.parseLong(value);
+					itemset.setTimestamp(timestamp);
+				} else if (element.equals("-1")) { // new itemset
+					long time = itemset.getTimestamp() + 1;
+					sequence.addItemset(itemset);
+					itemset = new Itemset();
+					itemset.setTimestamp(time);
+				} else if (element.equals("-2")) { // end sequence
+					sequences.add(sequence);
+					sequence = new Sequence();
+				} else {
+					// extract the value for an item
+					Item item = itemFactory.getItem(Integer.parseInt(element));
+					itemset.addItem(item);
+				}
 			}
 		}
+	}
+
+	public List<Sequence> getSequences() {
+		return sequences;
+	}
+
+	public void setSequences(List<Sequence> sequences) {
+		this.sequences = sequences;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder rs = new StringBuilder();
+		for (Sequence sequence : sequences) {
+			rs.append(sequence + "\n");
+		}
+		return rs.toString();
 	}
 }
