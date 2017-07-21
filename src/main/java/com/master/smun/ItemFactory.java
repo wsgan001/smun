c4602f8a46ee8d815e8c1140e2f7a616ec5c641f
Bui <bang.bui@capgemini.com>
2017-07-21 17:57:17 +0700

    update

--- src/main/java/com/master/smun/ItemFactory.java
+++ src/main/java/com/master/smun/ItemFactory.java
@@ -3,19 +3,11 @@ package com.master.smun;
 import java.util.HashMap;
 import java.util.Map;
 
-@SuppressWarnings("rawtypes")
-public class ItemFactory<T extends Comparable> {
-
-    /**
-     * The only constructor
-     */
-    public ItemFactory() {
-    }
-    
+public class ItemFactory {
     /**
      * The pool where all the items are kept
      */
-    private Map<T, Item> pool = new HashMap<T, Item>();
+    private Map<Integer, Item> pool = new HashMap<Integer, Item>();
 
     /**
      * Method to obtain the reference of an item if this has already been
@@ -23,7 +15,7 @@ public class ItemFactory<T extends Comparable> {
      * @param key the key of the item
      * @return the item
      */
-    public Item getItem(T key) {
+    public Item getItem(Integer key) {
         Item item = pool.get(key);
         if (item == null) {
             item = new Item(key);
