7c7bfb9ef307e6a8fabe97b3ea3e8dbabc4fc1bc
Bui <bang.bui@capgemini.com>
2017-07-21 17:58:44 +0700

    update

--- src/main/java/com/master/smun/Smun.java
+++ src/main/java/com/master/smun/Smun.java
@@ -1,8 +1,5 @@
 package com.master.smun;
 
-import java.io.IOException;
-
-
 /** 
  * @author bangbv
  */
@@ -10,12 +7,14 @@ public class Smun {
 	
 	public Item[] items;
 
-	void runAlgorithm(String fileName, double support, String output) throws IOException {
+	void runAlgorithm(String fileName, double support, String output) throws Exception {
 	// Scan DB
+	SequenceDatabase sdb = new SequenceDatabase();
+	sdb.scanDB1(fileName);
+	System.out.println(sdb.toString());
 	// Build tree
 	// project FUSP tree
 	// 
 		
-	}
-	
+	}	
 }
