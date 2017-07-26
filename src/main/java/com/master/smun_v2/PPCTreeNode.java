42e0d25d1fa85a043875c96f2244f3a6005410d2
Bui <bang.bui@capgemini.com>
2017-07-26 17:32:13 +0700

    update

--- src/main/java/com/master/smun_v2/PPCTreeNode.java
+++ src/main/java/com/master/smun_v2/PPCTreeNode.java
@@ -4,7 +4,7 @@ public class PPCTreeNode {
 	public int label;
 	public PPCTreeNode firstChild;
 	public PPCTreeNode rightSibling;
-	public PPCTreeNode labelSibling;
+	public PPCTreeNode labelSibling; // save for headtable link
 	public PPCTreeNode father;
 	public int count;
 	public int foreIndex;
