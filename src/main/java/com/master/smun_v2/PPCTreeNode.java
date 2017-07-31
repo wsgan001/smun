e140ba29047bc0d0cb18b87de15a182ced78e3dd
bangbv <bangbv@vng.com.vn>
2017-07-31 09:25:02 +0700

    update

--- src/main/java/com/master/smun_v2/PPCTreeNode.java
+++ src/main/java/com/master/smun_v2/PPCTreeNode.java
@@ -1,12 +1,15 @@
 package com.master.smun_v2;
 
+import java.util.List;
+
 public class PPCTreeNode {
 	public int label;
 	public PPCTreeNode firstChild;
 	public PPCTreeNode rightSibling;
 	public PPCTreeNode labelSibling; // save for headtable link
 	public PPCTreeNode father;
-	public int count;
+	public List<Integer> sequenceId;
+	//public int count;
 	public int foreIndex;
 	public int backIndex;
 }
