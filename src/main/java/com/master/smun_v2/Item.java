3b625988e9a9ec5e5a855969d4ec66f454029136
bangbv <bangbv@vng.com.vn>
2017-08-21 08:27:22 +0700

    update

--- src/main/java/com/master/smun_v2/Item.java
+++ src/main/java/com/master/smun_v2/Item.java
@@ -3,4 +3,9 @@ package com.master.smun_v2;
 public class Item {
 	public int id;
 	public int count;
+	
+	@Override
+	public String toString() {
+		return String.valueOf(id).toString()+":"+String.valueOf(count).toString();
+	}	
 }
