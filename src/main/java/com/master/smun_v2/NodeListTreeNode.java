22e7e81a1230ab261b773b876dc14d8cd20b5d9d
Bui <bang.bui@capgemini.com>
2017-08-23 17:31:40 +0700

    update

--- src/main/java/com/master/smun_v2/NodeListTreeNode.java
+++ src/main/java/com/master/smun_v2/NodeListTreeNode.java
@@ -1,6 +1,5 @@
 package com.master.smun_v2;
 
-import java.util.Set;
 
 /**
  * N-list
@@ -11,9 +10,10 @@ public class NodeListTreeNode {
 	public int label;
 	public NodeListTreeNode firstChild;
 	public NodeListTreeNode next;
-	public Set<Integer> sequenceId;
-	//public int support;
+	//public Set<Integer> sequenceId;
+	public int support;
 	public int NLStartinBf;
 	public int NLLength;
 	public int NLCol;
+	public int isSequence;
 }
