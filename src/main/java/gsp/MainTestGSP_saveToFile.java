7f20c976ca3f633a44460e8ee1bbc7282fff617c
bangbv <vanbang0208@gmail.com>
2017-12-29 14:21:50 +0700

    update

--- src/main/java/gsp/MainTestGSP_saveToFile.java
+++ src/main/java/gsp/MainTestGSP_saveToFile.java
@@ -16,7 +16,7 @@ public class MainTestGSP_saveToFile {
     public static void main(String[] args) throws IOException {
     	String output = ".//output.txt";
         // Load a sequence database
-        double support = 0.5, mingap = 0, maxgap = Integer.MAX_VALUE, windowSize = 0;
+        double support = 0.01, mingap = 0, maxgap = Integer.MAX_VALUE, windowSize = 0;
 
         boolean keepPatterns = true;
         boolean verbose=false;
@@ -28,20 +28,20 @@ public class MainTestGSP_saveToFile {
         AbstractionCreator abstractionCreator = AbstractionCreator_Qualitative.getInstance();
         SequenceDatabase sequenceDatabase = new SequenceDatabase(abstractionCreator);
 
-        sequenceDatabase.loadFile(fileToPath("input.txt"), support);
+        sequenceDatabase.loadFile(fileToPath("BMS1_spmf.txt"), support);
 
         AlgoGSP algorithm = new AlgoGSP(support, mingap, maxgap, windowSize,abstractionCreator);
 
 
-        System.out.println(sequenceDatabase.toString());
+        //System.out.println(sequenceDatabase.toString());
 
         //Change the file path in order to change the destination file
         algorithm.runAlgorithm(sequenceDatabase,keepPatterns,verbose, output, outputSequenceIdentifiers);
-        System.out.println(algorithm.getNumberOfFrequentPatterns()+ " frequent pattern found.");
+        //System.out.println(algorithm.getNumberOfFrequentPatterns()+ " frequent pattern found.");
 
-        System.out.println(algorithm.printedOutputToSaveInFile());
+        //System.out.println(algorithm.printedOutputToSaveInFile());
         
-        //System.out.println(algorithm.printStatistics());
+        System.out.println(algorithm.printStatistics());
     }
 
     public static String fileToPath(String filename) throws UnsupportedEncodingException {
