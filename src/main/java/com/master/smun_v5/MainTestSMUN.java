394a79c61d37941c146fb1ce6d926a18b51562bb
bangbv <vanbang0208@gmail.com>
2018-02-23 11:35:53 +0700

    update

--- src/main/java/com/master/smun_v5/MainTestSMUN.java
+++ src/main/java/com/master/smun_v5/MainTestSMUN.java
@@ -7,9 +7,14 @@ import java.net.URL;
 public class MainTestSMUN {
 
 	public static void main(String [] arg) throws IOException{
-		String input = fileToPath("input.txt");
-		String output = ".//output.txt";  // the path for saving the frequent sequences found		
-		double minsup = 0.5; // means a minsup of 2 transaction (we used a relative support)		
+		//String filename = "/input.txt";
+		//String filename = "/BIBLE.txt";
+		//String filename = "/BMS1_spmf.txt";
+		//String filename = "/Kosarak.txt";
+		String filename = "/FIFA.txt";
+		String input = fileToPath(filename);
+		String output = "/Users/bvbang/workspace/smun/src/main/resources/output_smun.txt";  // the path for saving the frequent sequences found		
+		double minsup = 0.3; // means a minsup of 2 transaction (we used a relative support)		
 		// Applying the algorithm
 		SMUN smun = new SMUN();
 		smun.runAlgorithm(input, minsup, output);
